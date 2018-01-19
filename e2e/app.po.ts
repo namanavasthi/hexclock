e33c28cac11405eddda4f9de555d0f5045a44a27
NAMAN AVASTHI <naman.avasthi@gmail.com>
2018-01-18 17:39:39 -0800

    small changes for heroku

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -2,7 +2,7 @@ import { browser, by, element } from 'protractor';
 
 export class AppPage {
   navigateTo() {
-    return browser.get('/');
+    return browser.get('https://hex-clock.herokuapp.com/');
   }
 
   getParagraphText() {
